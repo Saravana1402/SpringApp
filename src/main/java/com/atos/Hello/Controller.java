4c969118212d59321d281934e1c9043d2a88201e
Saravana D <34307939+Saravana1402@users.noreply.github.com>
2021-06-24 20:13:03 +0530

    Update Controller.java

--- src/main/java/com/atos/Hello/Controller.java
+++ src/main/java/com/atos/Hello/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-@RequestMapping("/")
+@RequestMapping("/hello")
 public String hello()
 {
 	return "Hello! This is Saravana B.E";
